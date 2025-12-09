c6dfbe6c3cc8d0b5f5c62d0f2848eab02f13ef13
Ange <odychristdahounliton@gmail.com>
2025-12-10 00:47:06 +0100

    New update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { PlusIcon, MenuIcon } from "lucide-react";
 import { Link, useNavigate } from "react-router";
 import { AppContent } from "../context/AppContext";
@@ -7,7 +7,8 @@ const Navbar = (props) => {
   const setState = props.setState;
   const navigate = useNavigate();
   const { userData, logout } = useContext(AppContent);
-  // const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [logoutOpen, setLogoutOpen] = useState(false);
 
   return (
     <>
@@ -28,46 +29,60 @@ const Navbar = (props) => {
               New Task
             </p>
           </div>
-          <div className="border border-cyan-500 rounded-lg p-2 shadow-sm shadow-cyan-600 hover:shadow-md cursor-pointer relative group">
-            <MenuIcon className="size-4 sm:size-7 text-cyan-400 font-bold"></MenuIcon>
-            <div className="hidden group-hover:flex flex-col gap-1 text-sm text-cyan-400 bg-slate-800 p-1 rounded-lg absolute left-6 top-8 sm:top-11">
-              <button
-                onClick={() => {
-                  setState("all");
-                }}
-                className="bg-slate-900 text-xs sm:text-sm border border-cyan-500 p-1 rounded-lg shadow-sm shadow-cyan-600 hover:shadow-md cursor-pointer"
-              >
-                All
-              </button>
-              <button
-                onClick={() => setState("open")}
-                className="bg-slate-900 text-xs sm:text-sm border border-cyan-500 p-1 rounded-lg shadow-sm shadow-cyan-600 hover:shadow-md cursor-pointer"
-              >
-                Open
-              </button>
-              <button
-                onClick={() => {
-                  setState("complete");
-                }}
-                className="bg-slate-900 text-xs sm:text-sm border border-cyan-500 p-1 rounded-lg shadow-sm shadow-cyan-600 hover:shadow-md cursor-pointer"
-              >
-                Complete
-              </button>
-            </div>
+          <div className="border border-cyan-500 rounded-lg p-2 shadow-sm shadow-cyan-600 hover:shadow-md cursor-pointer relative">
+            <MenuIcon
+              onClick={() => {
+                open ? setOpen(false) : setOpen(true);
+              }}
+              className="size-4 sm:size-7 text-cyan-400 font-bold"
+            ></MenuIcon>
+            {open && (
+              <div className="flex flex-col gap-1 text-sm text-cyan-400 bg-slate-800 p-1 rounded-lg absolute left-6 top-8 sm:top-11">
+                <button
+                  onClick={() => {
+                    setState("all");
+                  }}
+                  className="bg-slate-900 text-xs sm:text-sm border border-cyan-500 p-1 rounded-lg shadow-sm shadow-cyan-600 hover:shadow-md cursor-pointer"
+                >
+                  All
+                </button>
+                <button
+                  onClick={() => setState("open")}
+                  className="bg-slate-900 text-xs sm:text-sm border border-cyan-500 p-1 rounded-lg shadow-sm shadow-cyan-600 hover:shadow-md cursor-pointer"
+                >
+                  Open
+                </button>
+                <button
+                  onClick={() => {
+                    setState("complete");
+                  }}
+                  className="bg-slate-900 text-xs sm:text-sm border border-cyan-500 p-1 rounded-lg shadow-sm shadow-cyan-600 hover:shadow-md cursor-pointer"
+                >
+                  Complete
+                </button>
+              </div>
+            )}
           </div>
 
-          <div className="flex items-center justify-center bg-white rounded-full h-9 w-9 sm:w-12 sm:h-12 text-xl relative group cursor-pointer">
+          <div
+            onClick={() => {
+              logoutOpen ? setLogoutOpen(false) : setLogoutOpen(true);
+            }}
+            className="flex items-center justify-center bg-white rounded-full h-9 w-9 sm:w-12 sm:h-12 text-xl relative cursor-pointer"
+          >
             {userData && userData.name ? userData.name[0].toUpperCase() : "?"}
-            <div className="hidden absolute group-hover:block text-sm top-10 right-2 text-black bg-white p-2">
-              <ul className="w-24">
-                <li
-                  onClick={logout}
-                  className="bg-slate-200 rounded-lg cursor-pointer p-1 hover:bg-slate-300"
-                >
-                  Logout
-                </li>
-              </ul>
-            </div>
+            {logoutOpen && (
+              <div className="absolute text-sm top-10 right-2 text-black bg-white p-2">
+                <ul className="w-24">
+                  <li
+                    onClick={logout}
+                    className="bg-slate-200 rounded-lg cursor-pointer p-1 hover:bg-slate-300"
+                  >
+                    Logout
+                  </li>
+                </ul>
+              </div>
+            )}
           </div>
         </div>
       </header>
